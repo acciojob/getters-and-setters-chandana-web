669380b8e653ba5e08f2edb8bf544bac6d972e5b
chandana-web <143087553+chandana-web@users.noreply.github.com>
2025-01-27 08:16:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age) {
+    this._name = name; // Using a private property with an underscore
+    this._age = age;
+  }
 
-class Student extends Person {}
+  // Getter for name
+  get name() {
+    return this._name;
+  }
 
-class Teacher extends Person {}
+  // Setter for age
+  set age(newAge) {
+    if (newAge > 0) {
+      this._age = newAge;
+    } else {
+      console.error("Age must be a positive number.");
+    }
+  }
+}
+
+class Student extends Person {
+	study() {
+    console.log(`${this.name} is studying.`);
+  }
+}
+
+class Teacher extends Person {
+	teach() {
+    console.log(`${this.name} is teaching.`);
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
